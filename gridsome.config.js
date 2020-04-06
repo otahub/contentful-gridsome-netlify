5006d0a19c26dab31b665dc99ffde45491858ffe
otahub <hidenori.ota3@gmail.com>
2020-04-06 16:45:21 +0900

    Connect to Contentful

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  templates: {
-    // Add templates for content types here.
-    // Read more: https://gridsome.org/docs/templates/
-  },
-  plugins: [
-    {
-      use: '@gridsome/source-contentful',
-      options: {
-        space: 'SPACE_ID', // required
-        accessToken: 'ACCESS_TOKEN', // required
-        typeName: 'Contentful'
-      }
-    }
-  ]
+    templates: {
+        // Add templates for content types here.
+        // Read more: https://gridsome.org/docs/templates/
+    },
+    plugins: [
+        {
+            use: '@gridsome/source-contentful',
+            options: {
+                space: '3sm5b5m1wi2c', // required
+                accessToken: 'CJ7ap9JyT2CCD_8sr4mPzaaXBR5DG7p8-L-y-ZKvUJg', // required
+                host: 'cdn.contentful.com',
+                environment: 'master',
+                typeName: 'Contentful'
+            }
+        }
+    ]
 }
